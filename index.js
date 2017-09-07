4b7141c87fa5d333f2a937aaf1eac0b707ff88ef
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-09-07 10:25:35 +0100

    stop deleting all the jobs all the time, lost weren't getting run

--- index.js
+++ index.js
@@ -8,24 +8,41 @@ const health = new Health({service: 'cron_runner', http: true, interval: 30 * 10
 
 const tasks = []
 
-const scheduleService = (service) => {
-  let schedule = service.data.Spec.TaskTemplate.ContainerSpec.Labels['com.df.schedule']
-  console.log(`${schedule} ${service.data.Spec.Name}`)
-  tasks.push(cron.schedule(schedule, () => restartService(service)))
+const getServiceSchedule = service => service.data.Spec.TaskTemplate.ContainerSpec.Labels['com.df.schedule']
+const getServiceName = service => service.data.Spec.Name
+
+const scheduleService = (service) => tasks.push({
+  name: getServiceName(service),
+  schedule: getServiceSchedule(service),
+  job: cron.schedule(getServiceSchedule(service), () => restartService(service))
+})
+
+const removeOldTasks = (services) => {
+  let services_schedules = {}
+  _.each(services, service => services_schedules[getServiceName(service)] = getServiceSchedule(service))
+  _.each(tasks, (task, i) => {
+    if (!services_schedules[task.name] || services_schedules[task.name] !== task.schedule) {
+      console.log(`Removing ${task.name}`)
+      task.job.destroy()
+      tasks.splice(i, 1)
+    }
+  })
+  return services
 }
 
-const updateCronTable = () => {
-  console.log("Updating services")
-  _.each(tasks, (task, i) => {
-    if (task) {
-      task.destroy()
-    }
-    tasks.splice(i, 1)
-  })
-  Promise.resolve(docker.service.list())
-    .filter(service => service.data.Spec.TaskTemplate.ContainerSpec.Labels['com.df.cron2'] === 'true')
-    .map(scheduleService)
-}
+const log_current_tasks = () => console.log({'current tasks': tasks})
+
+const updateCronTable = () => Promise.resolve(docker.service.list())
+  .filter(service => service.data.Spec.TaskTemplate.ContainerSpec.Labels['com.df.cron2'] === 'true')
+  .tap(removeOldTasks)
+  .filter(noChangedServices)
+  .each(scheduleService)
+  .then(log_current_tasks)
+
+const noChangedServices = service => !_.find(tasks, {
+  name: getServiceName(service),
+  schedule: getServiceSchedule(service)
+})
 
 const restartService = service =>
   Promise.resolve(setServiceReplicas(service, 0))
